a428d15428ba39f7f80b881eea98a1126819a1aa
Parvati Yadav <parvatigitub@gmail.com>
2026-03-23 20:44:44 +0530

    first upload variable code

---
+++ variable.py
@@ -0,0 +1,27 @@
+# Variable
+# Defination of variable
+# ans:Variable is name that refers to the address of the memory location where store the value.
+# 2.why use the variable 
+# ans:Variables are used because where the data is stored in the memory,the value of the variable is taken.
+# 3. How is a variable stored in memory?
+#ans:Values ​​are stored in memory as objects.
+
+x=10   # x is variable and 10 is value
+print(x)
+#output: 10
+
+num=20
+print(num)
+#output: 20
+# in memory how to store the value
+#Memory:
+#[10]  ← object
+#x ----> 10
+
+num=30  #same variable me different value store karenge toh pehle wala value override ho jayega
+print(num)
+#output: 30
+
+name="parvati"  # ye variale me string value store kiya gaya hai
+print(name)
+#output: parvati
